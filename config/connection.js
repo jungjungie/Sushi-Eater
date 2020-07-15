3f7684dc3b6637022e877fcb9ca51b48f29c37a5
Esther Min <estherjmin@gmail.com>
2020-07-14 22:36:33 -0400

    added code to deploy to heroku with jawsDB remote database

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // MySQL connection setup
 var mysql = require('mysql');
 require('dotenv').config();
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.DB_PASS,
-  database: "sushi_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASS,
+    database: "sushi_db"
+  });
+}
 
 // Makes connection
 connection.connect(function(err) {
